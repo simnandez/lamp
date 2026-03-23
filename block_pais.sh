cd99f92be33525cf2bb95b5e2231aab21a8fa90b
Juanjo Menent <jmenent@simnandez.ez>
2026-03-23 18:38:58 +0100

    a posicion 3

--- block_pais.sh
+++ block_pais.sh
@@ -9,9 +9,9 @@ for country in "$@"; do
     ipset restore
 
     if ! iptables -C INPUT -m set --match-set "pais-$country" src -j DROP 2>/dev/null; then
-        # IMPORTANTE: Insertamos en la posición 2 para no pisar la Whitelist de Google
-        iptables -I INPUT 2 -m set --match-set "pais-$country" src -j DROP
-        echo "Regla de IPTables aplicada para $country en posición #2."
+        # IMPORTANTE: Insertamos en la posición 3 para no pisar la Whitelist de Google
+        iptables -I INPUT 3 -m set --match-set "pais-$country" src -j DROP
+        echo "Regla de IPTables aplicada para $country en posición #3."
     fi
 done
 netfilter-persistent save
